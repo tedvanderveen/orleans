efe42052a256e59622f1d91e5ad7c3e0d4c2d236
ReubenBond <a-rebond@microsoft.com>
2017-12-20 17:19:14 +1100

    Add single-threaded client-silo ping benchmark

---
+++ test/Benchmarks/Benchmarks/Ping/SequentialPingBenchmark.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using Orleans.TestingHost;
+using BenchmarkGrainInterfaces.Ping;
+using Benchmarks.Serialization;
+
+namespace Benchmarks.Ping
+{
+    public class SequentialPingBenchmarkConfig : ManualConfig
+    {
+        public SequentialPingBenchmarkConfig()
+        {
+            Add(new MemoryDiagnoser());
+        }
+    }
+
+    [Config(typeof(SequentialPingBenchmarkConfig))]
+    public class SequentialPingBenchmark : IDisposable 
+    {
+        private readonly TestCluster host;
+        private readonly IPingGrain grain;
+
+        public SequentialPingBenchmark()
+        {
+            var options = new TestClusterOptions();
+            host = new TestCluster(options);
+            host.Deploy();
+            grain = host.Client.GetGrain<IPingGrain>(Guid.NewGuid().GetHashCode());
+            grain.Run().GetAwaiter().GetResult();
+        }
+        
+        [Benchmark]
+        public Task Ping() => grain.Run();
+        
+        public void Dispose()
+        {
+            host.StopAllSilos();
+        }
+    }
+}
